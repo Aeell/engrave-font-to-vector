1b5f6a7f76ec2222292b0d5fcebe7ca93800cd68
CNC User <cnc@example.com>
2025-09-23 12:47:28 +0200

    Move web files to root for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'web',
   base: '/engrave-font-to-vector/',
   build: {
-    outDir: '../dist-web',
+    outDir: 'dist',
   },
 });
